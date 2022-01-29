db396329114c1a4eb77bbec6be809d6768ea0a4d
Madhav Rathi <93340695+madhavrathi16@users.noreply.github.com>
2022-01-30 00:27:54 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
 // import using require
-
+const Shape =require("./shape")
 // declare class
+class Circle extends Shape{
+  super(radius){
+    this.radius= radius;
+  }
 
+     calculateArea(){
+    console.log('Area');
+     }
+
+}
+module.exports = Circle;
 
 // export class using module.exports
